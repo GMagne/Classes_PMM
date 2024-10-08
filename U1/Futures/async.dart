c814a2f38fd074a6034a49a02e177307b908780c
Guillem <99399044+GMagne@users.noreply.github.com>
2024-10-08 13:44:00 +0200

    09/10

---
+++ U1/Futures/async.dart
@@ -0,0 +1,37 @@
+// Tasques asíncrones i com esperar el seu resultat
+
+String getID(String id) {
+  return 'ID de Jaume: $id';
+}
+
+// Retorna un future, per això ens donaria error
+/*
+String getNom(String id) async{
+  return 'ID de Jaume: $id';
+}
+
+// i com ho podriem arreglar? Afegint que el resultar serà una "promesa"
+*/
+Future<String> getIDAsync(String id) async {
+  // Map<String,dynamic>
+  return 'ID de Jaume: $id';
+}
+
+//void main() async{ Els mètodes superiors també han de ser asincrons
+void main() async {
+  print('Inici del programa');
+  print(getID('1'));
+
+  print(getIDAsync('2')); //await
+
+  getIDAsync('3').then((data) => print(data)); //then
+
+  getIDAsync('4').then(print);
+
+  final nom = await getIDAsync('5'); //await
+  print(nom);
+  getIDAsync('6').then(print);
+  // I si volem que una funció s'executi en segons pla
+  // però que acabi abans de fí de programa?
+  print('Fí del programa');
+}
